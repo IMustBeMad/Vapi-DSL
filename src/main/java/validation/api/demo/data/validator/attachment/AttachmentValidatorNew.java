7fc13e11b3c390fe6042b90b72c34650caefa5fa
k.poltoratskyi <k.poltoratskyi@controlpay.com>
2019-04-30 17:27:12 +0300

    attachment validation

--- src/main/java/validation/api/demo/data/validator/attachment/AttachmentValidatorNew.java
+++ src/main/java/validation/api/demo/data/validator/attachment/AttachmentValidatorNew.java
@@ -1,16 +1,31 @@
 package validation.api.demo.data.validator.attachment;
 
+import validation.api.demo.data.common.Attachment;
 import validation.api.demo.data.common.Claim;
+import validation.api.demo.data.common.Doc;
 import validation.api.demo.validation.Validation;
-import validation.api.demo.validation.domain.number.LongConditions;
+import validation.api.demo.validation.domain.list.ListConditions;
+
+import java.util.Collections;
+import java.util.List;
 
 public class AttachmentValidatorNew {
 
-    public void validate() {
-        Validation.verifyIf(new Claim())
-                  .inspecting(Claim::getId, () -> LongConditions.isEqualTo(42L))
+    public void validate(Claim claim, List<Doc> docs) {
+        List<Attachment> attachments = claim.getAttachments();
+
+        Validation.verifyIf(attachments)
+                  .isEmpty("test")
                   .or()
-                  .isEqualTo(new Claim(), "error")
+                  .withTerm(verifyAttachmentOnSubmit(clientId, file, attachment, docs))
                   .failIfNoneGroupMatch();
+    }
+
+    private List<Attachment> getClaimAttachments(Long claimId) {
+        return Collections.emptyList();
+    }
+
+    private Long getCurrentClientId() {
+        return 42L;
     }
 }
