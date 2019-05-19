db73b2fb019166d014bef676839978ce1b32cd90
IMustBeMad <kpoltoratskyi@gmail.com>
2019-05-19 23:25:37 +0300

    fix tests

--- src/test/java/validation/api/demo/data/ValidatorListTest.java
+++ src/test/java/validation/api/demo/data/ValidatorListTest.java
@@ -4,6 +4,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import validation.api.demo.validation.Validation;
+import validation.api.demo.validation.dict.TerminationMode;
+import validation.api.demo.validation.domain.number.LongConditions;
 
 import java.util.List;
 
@@ -15,21 +18,23 @@ public class ValidatorListTest {
     public void should_beNoErrors_when_eachElementIsCorrect() {
         List<Long> longs = List.of(1L, 2L, 3L, 42L);
 
-//        Validation.verifyIf(longs)
-//                  .ofSize(4, "incorrect.size")
-//                  .inspecting(it -> it.get(3), it -> it == 42L, "not.equal")
-//                  .each(LongConditions.isGt(0L), "not.greater")
-//                  .failOn(TerminationMode.FIRST_ERROR_ENCOUNTERED);
+        Validation.verifyIf(longs)
+                  .ofSize(4).onError("incorrect.size")
+                  .inspecting(it -> it.get(3), it -> it == 42L).onError("not.equal")
+                  .each(LongConditions.isGt(0L)).onError("not.greater")
+                  .failOn(TerminationMode.FIRST_ERROR_ENCOUNTERED)
+                  .examine();
     }
 
     @Test
     public void should_raiseAnError_when_eachElementIsNotCorrect() {
         List<Long> longs = List.of(1L, 2L, 3L, 42L);
 
-//        Validation.verifyIf(longs)
-//                  .ofSize(4, "incorrect.size")
-//                  .inspecting(it -> it.get(3), it -> 42L == it, "not.equal")
-//                  .each(LongConditions.isGt(42L), "not.greater")
-//                  .failOn(TerminationMode.FIRST_ERROR_ENCOUNTERED);
+        Validation.verifyIf(longs)
+                  .ofSize(4).onError("incorrect.size")
+                  .inspecting(it -> it.get(3), it -> 42L == it).onError("not.equal")
+                  .each(LongConditions.isGt(42L)).onError("not.greater")
+                  .failOn(TerminationMode.FIRST_ERROR_ENCOUNTERED)
+                  .examine();
     }
 }
