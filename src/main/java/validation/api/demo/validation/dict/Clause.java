3370f8650cd17433d945d6b11b40c1cc4210a46c
k.poltoratskyi <k.poltoratskyi@controlpay.com>
2019-09-04 10:40:48 +0300

    docs

--- src/main/java/validation/api/demo/validation/dict/Clause.java
+++ src/main/java/validation/api/demo/validation/dict/Clause.java
@@ -1,5 +1,18 @@
 package validation.api.demo.validation.dict;
 
+/**
+ * {@code Clause} enum is used to link condition serial in {@link validation.api.demo.validation.common.LinkedCondition}
+ * object.
+ */
 public enum Clause {
-    AND, OR
+
+    /**
+     * Represents logical "and"(&&) for conditions.
+     */
+    AND,
+
+    /**
+     * Represents logical "or"(||) for conditions.
+     */
+    OR
 }
