4afb67001745f811c3702e96b20f7b98cba79b18
k.poltoratskyi <k.poltoratskyi@controlpay.com>
2019-09-26 11:56:53 +0300

    main

---
+++ src/main/java/validation/VapiDSLApplication.java
@@ -0,0 +1,12 @@
+package validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VapiDSLApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VapiDSLApplication.class, args);
+    }
+}
