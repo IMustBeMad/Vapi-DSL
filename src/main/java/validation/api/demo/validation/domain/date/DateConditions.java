e99b60396577b04c7faaf695c338cbf907d95e2f
k.poltoratskyi <k.poltoratskyi@controlpay.com>
2019-09-26 13:36:49 +0300

    datetime conditions

--- src/main/java/validation/api/demo/validation/domain/date/DateConditions.java
+++ src/main/java/validation/api/demo/validation/domain/date/DateConditions.java
@@ -3,6 +3,7 @@ package validation.api.demo.validation.domain.date;
 import validation.api.demo.validation.common.SingleCondition;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class DateConditions {
 
@@ -12,5 +13,29 @@ public class DateConditions {
 
     public static SingleCondition<LocalDate> isBefore(LocalDate otherDate) {
         return new SingleCondition<>(it -> it.isBefore(otherDate));
+    }
+
+    public static SingleCondition<LocalDate> isAfterOrEqual(LocalDate otherDate) {
+        return new SingleCondition<>(it -> it.isAfter(otherDate) || it.isEqual(otherDate));
+    }
+
+    public static SingleCondition<LocalDate> isBeforeOrEqual(LocalDate otherDate) {
+        return new SingleCondition<>(it -> it.isBefore(otherDate) || it.isEqual(otherDate));
+    }
+
+    public static SingleCondition<LocalDateTime> isAfter(LocalDateTime otherDate) {
+        return new SingleCondition<>(it -> it.isAfter(otherDate));
+    }
+
+    public static SingleCondition<LocalDateTime> isBefore(LocalDateTime otherDate) {
+        return new SingleCondition<>(it -> it.isBefore(otherDate));
+    }
+
+    public static SingleCondition<LocalDateTime> isAfterOrEqual(LocalDateTime otherDate) {
+        return new SingleCondition<>(it -> it.isAfter(otherDate) || it.isEqual(otherDate));
+    }
+
+    public static SingleCondition<LocalDateTime> isBeforeOrEqual(LocalDateTime otherDate) {
+        return new SingleCondition<>(it -> it.isBefore(otherDate) || it.isEqual(otherDate));
     }
 }
